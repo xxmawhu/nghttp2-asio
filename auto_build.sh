d481a41540ef1aaaf4e29a1f2937ef3d3ca13b7e
MA Xin-Xin <xxmawhu@gmail.com>
2025-02-05 21:25:40 +0800

    fix

--- auto_build.sh
+++ auto_build.sh
@@ -3,11 +3,11 @@ cd build
 cfg=' -DLIBNGHTTP2_INCLUDE_DIR=/opt/nghttp2-1.64.0/include/ -DLIBNGHTTP2_LIBRARY=/opt/nghttp2-1.64.0/lib/libnghttp2.a -DCMAKE_INSTALL_PREFIX=/opt/nghttp2-asio -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc'
 cmake .. -D ENABLE_STATIC_LIB=on -D ENABLE_SHARED_LIB=off ${cfg}
 
-cmake --build .
+cmake --build . -j
 sudo cmake --install .
 make clean
 
 cfg=' -DLIBNGHTTP2_INCLUDE_DIR=/opt/nghttp2-1.64.0/include/ -DLIBNGHTTP2_LIBRARY=/opt/nghttp2-1.64.0/lib/libnghttp2.so -DCMAKE_INSTALL_PREFIX=/opt/nghttp2-asio -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc'
 cmake .. -D ENABLE_STATIC_LIB=off -D ENABLE_SHARED_LIB=on ${cfg}
-cmake --build .
+cmake --build . -j
 sudo cmake --install .
