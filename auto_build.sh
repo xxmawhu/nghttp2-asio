690d3e08c4c84135efc2f0498c7e141f7d5a1815
MA Xin-Xin <xxmawhu@gmail.com>
2025-02-05 21:13:03 +0800

    update

--- auto_build.sh
+++ auto_build.sh
@@ -1,4 +1,4 @@
-mikdir build -p
+mkdir build -p
 cd build
 cfg=' -DLIBNGHTTP2_INCLUDE_DIR=/opt/nghttp2-1.64.0/include/ -DLIBNGHTTP2_LIBRARY=/opt/nghttp2-1.64.0/lib/libnghttp2.a -DCMAKE_INSTALL_PREFIX=/opt/nghttp2-asio -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc'
 cmake .. -D ENABLE_STATIC_LIB=on -D ENABLE_SHARED_LIB=off ${cfg}
